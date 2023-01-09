8b7bb89e46f05a4d7dda14ee468f85c11026c53a
chenglongcl <298115800@qq.com>
2023-01-09 15:43:54 +0800

    feat(options.go): 新增配置项

--- options.go
+++ options.go
@@ -1,10 +1,11 @@
 package jpush
 
 type Options struct {
-	SendNo          int    `json:"sendno,omitempty"`
-	TimeLive        int    `json:"time_to_live,omitempty"`
-	OverrideMsgId   int64  `json:"override_msg_id,omitempty"`
-	ApnsProduction  bool   `json:"apns_production"`
-	ApnsCollapseId  string `json:"apns_collapse_id,omitempty"`
-	BigPushDuration int    `json:"big_push_duration,omitempty"`
+	SendNo            int                               `json:"sendno,omitempty"`
+	TimeLive          int                               `json:"time_to_live,omitempty"`
+	OverrideMsgId     int64                             `json:"override_msg_id,omitempty"`
+	ApnsProduction    bool                              `json:"apns_production"`
+	ApnsCollapseId    string                            `json:"apns_collapse_id,omitempty"`
+	BigPushDuration   int                               `json:"big_push_duration,omitempty"`
+	ThirdPartyChannel map[string]map[string]interface{} `json:"third_party_channel,omitempty"`
 }
